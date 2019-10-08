a57c6047cf5f5e77790b72029aaecc675aefa7e5
krislowg <krislowg@gmail.com>
2019-10-07 20:58:38 -0400

    Check Availability button added

--- src/ResortArenaPalace/Controller.java
+++ src/ResortArenaPalace/Controller.java
@@ -1,12 +1,16 @@
 package ResortArenaPalace;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 
 public class Controller {
 
     @FXML
     private Tab tab_home;
+
+    @FXML
+    private Button btn_ckAv;
 
     @FXML
     private Tab tab_wellness;
