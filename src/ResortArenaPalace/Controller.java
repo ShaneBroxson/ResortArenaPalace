606067a5afd31c852c5ad6245a42c0c97dab5986
krislowg <krislowg@gmail.com>
2019-10-08 23:00:57 -0400

    Changing scenes from Landing Page to Room Availability through "check availability button".

--- src/ResortArenaPalace/Controller.java
+++ src/ResortArenaPalace/Controller.java
@@ -1,24 +1,40 @@
 package ResortArenaPalace;
 
+import java.awt.Image;
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
+import javafx.stage.Stage;
 
 public class Controller {
 
-    @FXML
-    private Tab tab_home;
+  @FXML
+  private Tab tab_home;
 
-    @FXML
-    private Button btn_ckAv;
+  @FXML
+  private Button btn_ckAv;
 
-    @FXML
-    private Tab tab_wellness;
+  @FXML
+  private Tab tab_wellness;
 
-    @FXML
-    private Tab tab_events;
+  @FXML
+  private Tab tab_events;
 
-    @FXML
-    private Tab tab_restaurant;
+  @FXML
+  private Tab tab_restaurant;
 
+  public void changeScreenRoomAv(ActionEvent actionEvent) throws IOException {
+    Parent roomAvailableParent = FXMLLoader.load(getClass().getResource("RoomAvailability.fxml"));
+    Scene roomAvScene = new Scene(roomAvailableParent);
+
+    Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    window.setScene(roomAvScene);
+    window.show();
+  }
 }
---
+++ src/ResortArenaPalace/RoomAvailability.java
@@ -0,0 +1,11 @@
+package ResortArenaPalace;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class RoomAvailability {
+
+  @FXML
+  private AnchorPane lbl_RoomAv;
+
+}
