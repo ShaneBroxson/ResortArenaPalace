61294ae560b641a64fe31e553bf1b187cb2c3f80
ImanEssaghir <essaghir.iman@gmail.com>
2019-10-17 15:49:16 -0400

    Back button trial 1, in comments

--- src/ResortArenaPalace/PaymentAccount.java
+++ src/ResortArenaPalace/PaymentAccount.java
@@ -2,4 +2,18 @@ package ResortArenaPalace;
 
 public class PaymentAccount {
 
+    // back button trial 1
+
+    /*@FXML
+    void changePaytoRA(ActionEvent event) throws IOException {
+        Parent roomAvailabilityParent = FXMLLoader.load(getClass().getResource("RoomAvailability.fxml"));
+        Scene roomAv = new Scene(roomAvailabilityParent);
+
+        Stage returnRA = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        returnRA.setScene(roomAv);
+        returnRA.show();
+    }*/
+
+
+
 }
