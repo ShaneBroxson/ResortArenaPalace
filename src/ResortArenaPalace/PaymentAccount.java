fc2ef48e774062974eff69c3afa4162da11dd445
krislowg <krislowg@gmail.com>
2019-10-17 21:36:05 -0400

    "Go back" button added. Change scene from the payment methods scene to the Room Availability.

--- src/ResortArenaPalace/PaymentAccount.java
+++ src/ResortArenaPalace/PaymentAccount.java
@@ -1,4 +1,15 @@
 package ResortArenaPalace;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 //Payment and User Account
 public class PaymentAccount {
 
@@ -13,6 +24,20 @@ public class PaymentAccount {
         returnRA.setScene(roomAv);
         returnRA.show();
     }*/
+
+
+  @FXML
+  private Button btn_GoBackRoomA;
+
+  @FXML
+  void changePayToRoomA(ActionEvent event) throws IOException {
+    Parent payParent = FXMLLoader.load(getClass().getResource("RoomAvailability.fxml"));
+    Scene payScene = new Scene(payParent);
+
+    Stage payWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    payWindow.setScene(payScene);
+    payWindow.show();
+  }
 
 
 
