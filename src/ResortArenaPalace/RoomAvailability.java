c601f985bfe241b60b96f35fd625a2500913454d
ImanEssaghir <essaghir.iman@gmail.com>
2019-10-19 22:10:02 -0400

    Add Underwater suite, Superior double queen room,Grand Arena suite, plus selection buttons

--- src/ResortArenaPalace/RoomAvailability.java
+++ src/ResortArenaPalace/RoomAvailability.java
@@ -8,41 +8,77 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class RoomAvailability {
 
-  @FXML
-  private AnchorPane lbl_RoomAv;
+    @FXML
+    private AnchorPane lbl_RoomAv;
 
-  @FXML
-  private Button btn_GoHomeRA;
+    @FXML
+    private AnchorPane room1;
 
-  @FXML
-  private AnchorPane room1;
+    @FXML
+    private TextArea r1Details;
 
-  @FXML
-  private Button btn_Select1rm;
+    @FXML
+    private TextArea r3Details;
 
-  @FXML
-  void changeR1toPay(ActionEvent event) throws IOException {
-    Parent paymentAccountParent = FXMLLoader.load(getClass().getResource("PaymentAccount.fxml"));
-    Scene roomAvScene = new Scene(paymentAccountParent);
+    @FXML
+    private Label room3;
 
-    Stage payWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    payWindow.setScene(roomAvScene);
-    payWindow.show();
-  }
+    @FXML
+    private Label room2;
 
-  @FXML
-  void changeRAToHome(ActionEvent event) throws IOException {
-    Parent roomAvHParent = FXMLLoader.load(getClass().getResource("LandingPage.fxml"));
-    Scene homePScene = new Scene(roomAvHParent);
+    @FXML
+    private TextArea r2Details;
 
-    Stage homePWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    homePWindow.setScene(homePScene);
-    homePWindow.show();
-  }
+    @FXML
+    private Label room4;
+
+    @FXML
+    private Label room;
+
+    @FXML
+    private Button btn_Select1rm;
+
+    @FXML
+    private Button btn_GoHomeRA;
+
+    @FXML
+    private Button btn_Select3rm;
+
+    @FXML
+    private Button btn_Select2rm;
+
+    @FXML
+    private TextArea r4Details;
+
+    @FXML
+    private Button btn_Select4rm;
+
+
+    @FXML
+    void changeR1toPay(ActionEvent event) throws IOException {
+        Parent paymentAccountParent = FXMLLoader.load(getClass().getResource("PaymentAccount.fxml"));
+        Scene roomAvScene = new Scene(paymentAccountParent);
+
+        Stage payWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        payWindow.setScene(roomAvScene);
+        payWindow.show();
+    }
+
+     @FXML
+    void changeRAToHome(ActionEvent event) throws IOException {
+        Parent roomAvHParent = FXMLLoader.load(getClass().getResource("LandingPage.fxml"));
+        Scene homePScene = new Scene(roomAvHParent);
+
+        Stage homePWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        homePWindow.setScene(homePScene);
+        homePWindow.show();
+    }
 
 }
